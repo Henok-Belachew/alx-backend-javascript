26b3e78235f229835f37bd85d9d49192dc4d9676
Henok Belachew <henokbelachewstu@gmail.com>
2023-06-30 22:47:29 +0300

    another bug fixed

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: employeesList,
-    getNumberOfDepartments(employeesList) {
+    allEmployees: {...employeesList},
+    getNumberOfDepartments() {
       return Object.values(employeesList).length;
     },
   };
