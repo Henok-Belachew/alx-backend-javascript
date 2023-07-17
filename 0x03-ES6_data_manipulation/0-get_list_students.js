209df105b82be8b8fee4ff245edd6c24912ebbdd
Henok Belachew <henokbelachewstu@gmail.com>
2023-07-17 20:34:49 +0300

    done with manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,12 @@
+/**
+ * Retrieves a list of students.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 export default function getListStudents() {
-  const Students = [
-    {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco'},
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia'},
-    {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Francisco'},
-  ]
-  ;
-  return Students;
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
